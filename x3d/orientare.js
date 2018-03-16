3adf08d32e53b1c2a8b81a8e61af769dfd2192d1
dianamaris9 <diddymaris93@gmail.com>
2018-03-16 18:51:26 +0200

    axe orientare 4

--- x3d/orientare.js
+++ x3d/orientare.js
@@ -2,4 +2,6 @@ window.addEventListener("deviceoreintation", on_device_orientation);
 
 function on_device_orientation(e){
 	document.getElementById(id_orientation).innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	var viewpoint = document.getElementById(id_viewpoint);
+	viewpoint.setAttribute("orientation", "0 0 1 " + e.alpha * Math.PI / 180);
 }
